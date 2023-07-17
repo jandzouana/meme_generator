4e57c7371ae37b516280a9b6927a13548b631999
Jessica Andzouana <jandzouana@users.noreply.github.com>
2023-07-16 21:58:23 -0700

    Added first files

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[hash].[ext]',
+                            outputPath: 'images',
+                        },
+                    },
+                ],
+            },
+        ]
+    }
+};
