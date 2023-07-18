0e47ab280d1210834ba5308f4535b6c458f58b6c
jandzouana <jandzouana@alcacruz.com>
2023-07-17 19:34:02 -0700

    updated webpack files, added favicon

--- src/meme/components/app.js
+++ src/meme/components/app.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../styles.css';
 import Header from "./header";
 import Meme from "./meme";
+import favicon from "../assets/troll.ico";
 
 const App = () => {
+    useEffect(()=>{
+        document.getElementById("favicon").href = favicon;
+    }, [])
     return(
         <>
             <Header/>
